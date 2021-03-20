aad8fbd95a6933f3de2d9397870633dd6958f747
Hyun083 <jh9501@gmail.com>
2021-03-20 21:23:10 +0900

    JH [gonggong.java] 추가.

---
+++ src/main/java/com/example/myproject/controller/gonggong.java
@@ -0,0 +1,45 @@
+package com.example.myproject.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class gonggong {
+
+    @GetMapping("/apitest")
+    public String testhttp(){
+
+        StringBuffer result = new StringBuffer();
+
+        try{
+            String urlstr ="https://open.jejudatahub.net/api/proxy/Dtb18ta1btbD1Da1a81aaDttab6tDabb/{your_appkey}?addressDong=구좌읍";
+
+            URL url = new URL(urlstr);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String returnLine;
+
+            result.append("<xmp>");
+            while ((returnLine = br.readLine()) !=null){
+                result.append(returnLine+ "\n");
+            }
+            urlConnection.disconnect();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return result+"</xmp>";
+    }
+}
