9c2dea2d65a38371a23431f0eead4eea3af36760
Deokkyu <keusc784@gmail.com>
2021-04-06 00:59:29 +0900

    DK 로그인 삭제

--- src/main/java/com/example/myproject/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.repository;
-
-import com.example.myproject.model.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Integer> {
-  Token findTop1ByToken(String token);
-}
